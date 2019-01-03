fa62aa638c678a3efadc23a501a2e80b822a8133
Raul Bravo <br4ca@hotmail.com>
2019-01-03 22:32:52 +0000

    Added repro for tabcontrol bug

---
+++ AvaloniaTabControl/SampleView.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace AvaloniaTabControl
+{
+    public class SampleView : UserControl
+    {
+        public SampleView()
+        {
+            this.InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
